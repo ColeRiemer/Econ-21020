e2feb8f49cecd4e8b7a3d4d049678f6d4ee2e642
thomaswiemann <wiemann@uchicago.edu>
2021-03-22 21:51:53 +0100

    add html and ipynb for problem set 0

--- Problem_Set_0.R
+++ Problem_Set_0.R
@@ -7,7 +7,7 @@
 library(ggplot2) # for plots
 
 # Source custome code
-source("mymethods/source_mymethods.R")
+source("mymethods/ols.R")
 
 # Data =========================================================================
 
@@ -24,10 +24,10 @@ head(df, 10)
 
 dim(df) # 1620 observations with 4 variables
 
-# Calculate summary statistics for each variable. First, use the `summary' 
-#     command. Then, replicate the mean with the `apply' function. Also 
+# Calculate summary statistics for each variable. First, use the `summary`
+#     command. Then, replicate the mean with the `apply` function. Also 
 #     calculate the 10th, and 90th quantile of each  vaiable, as well as their 
-#     standard deviation. Combine the summary statitics in a data.frame.
+#     standard deviation. Combine the summary statistics in a dataframe.
 
 summary(df)
 
@@ -53,16 +53,16 @@ table(df$X.3) / dim(df)[1] # relative frequencies
 
 # As a last part of the analysis here, we characterize the conditional 
 #     distribution distribution of y given X via linear regression. R's built-in 
-#     command for linear regression is `lm', which we use here first. To obtain
+#     command for linear regression is `lm`, which we use here first. To obtain
 #     the coefficient values and corresponding standard errors, we use the 
-#     `summary' method (also built-in).
+#     `summary` method (also built-in).
 
 fit_lm <- lm(y ~ ., data = df) # linear regression of y on X and a constant
 summary(fit_lm) # get coefficient values and standard errors
 
 # Let us briefly check whether the regression output is sensible. For example, 
 #     let's plot the regression residuals against our three variables X1-X3. We 
-#     can use R's build in `plot' function for this. 
+#     can use R's build in `plot` function for this. 
 
 plot(x = df$X.1, y = fit_lm$residuals, 
      xlab = "X.1", ylab = expression(epsilon))
@@ -72,7 +72,7 @@ plot(x = df$X.3, y = fit_lm$residuals,
      xlab = "X.3", ylab = expression(epsilon))
 
 # For better plots with more customization options, the reccomended package is 
-#     ggplot2. In the below, the third residual plot is reproduced and exported 
+#     `ggplot2`. In the below, the third residual plot is reproduced and exported 
 #     as a .pdf file (e.g., so that it can be included in a LaTeX document.)
 
 pdf("Results/Problem_Set_0/Figures/plt_residualX3.pdf", 
@@ -85,7 +85,7 @@ ggplot(df_plt, aes(x = X.3, y = eps)) +
   xlab("X.3") +
   theme_classic(base_size = 30)
 
-dev.off() # make sure to include this when using `pdf'
+dev.off() # make sure to include this when using `pdf`
 
 # The third plot, where we consider X3, suggests that we did not correctly 
 #     capture the relationship between y and X3 with the linear model considered 
@@ -93,7 +93,7 @@ dev.off() # make sure to include this when using `pdf'
 #     X2, and the discrete X3. To allow for nonlinearities, it may be better to 
 #     use indicator variables for different levels of X3. To do so, first set 
 #     the variable type to factor. R will then include indicator variables when
-#     calling `lm'.
+#     calling `lm`.
 
 df$X.3 <- as.factor(df$X.3)
 fit_lm2 <- lm(y ~ ., data = df)
@@ -113,23 +113,23 @@ df_res <- data.frame(summary_2$coefficients)
 write.csv(df_sumstats, file="Results/Problem_Set_0/Tables/summary_stats.csv")
 write.csv(df_res, file="Results/Problem_Set_0/Tables/regression_res.csv")
 
-# Optional: Creating your own lm object ========================================
+# Optional: Creating your own `lm` object ======================================
 
 # One of the benefits of R over readily packaged statistical software (such as 
 #     Stata or SPSS) is the ease with which statistical estimators can be 
 #     implemented (or improved upon) by hand. To see how easy this can be, try 
-#     writing your own `lm' function. I've written one called `ols', which 
+#     writing your own `lm` function. I've written one called `ols`, which 
 #     supports basic regression analysis of a univariate outcome on a set of 
 #     regressors stored in a matrix. It does not have the helpful interface of 
-#     the `lm' object. See the file stored as "R Code/ols.R" for details.
+#     the `lm` object. See the file stored as "R Code/ols.R" for details.
 
 # First, let's get the full regression matrix where X3 is included as a set of 
 #     indicator variables.
 X_mat <- model.matrix(y ~ ., data = df)
 head(X_mat, 10) # see the last columns for the indicator variables
 
-# We may then regress y on X_mat as before. I've also written a `summary' method
-#     for my ols object. The coefficient values and standard errors should be 
+# We may then regress y on X_mat as before. I've also written a `summary` method
+#     for my `ols` object. The coefficient values and standard errors should be 
 #     the same as before. 
 fit_ols <- ols(y = df$y, X = X_mat)
 summary(fit_ols)
@@ -140,7 +140,7 @@ summary(fit_ols)
 #     a package, they are seldomly optimized for the task at hand. To save 
 #     computational time, your own implementations can be of benefit -- even for
 #     estimators as simple as linear regression. See the example below, where 
-#     `ols' is faster than `lm'.
+#     `ols` is faster than `lm`.
 
 N <- 1000000 # set large sample size to see difference in time 
 x <- rnorm(N) # simulate regressor 
